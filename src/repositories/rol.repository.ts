7af21b13235215156c312f8c5e4bbb0647eb6b88
eric <eric.500.garcia@gmail.com>
2025-09-30 18:58:05 +0200

    se crean los elementos datasourc3e hasta controllers par el mas de usuarios admin

---
+++ src/repositories/rol.repository.ts
@@ -0,0 +1,22 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
+import {MongodbDataSource} from '../datasources';
+import {Rol, RolRelations, Usuario} from '../models';
+import {UsuarioRepository} from './usuario.repository';
+
+export class RolRepository extends DefaultCrudRepository<
+  Rol,
+  typeof Rol.prototype.id,
+  RolRelations
+> {
+
+  public readonly ta_asociado: HasManyRepositoryFactory<Usuario, typeof Rol.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongodb') dataSource: MongodbDataSource, @repository.getter('UsuarioRepository') protected usuarioRepositoryGetter: Getter<UsuarioRepository>,
+  ) {
+    super(Rol, dataSource);
+    this.ta_asociado = this.createHasManyRepositoryFactoryFor('ta_asociado', usuarioRepositoryGetter,);
+    this.registerInclusionResolver('ta_asociado', this.ta_asociado.inclusionResolver);
+  }
+}
